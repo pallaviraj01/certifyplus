efd0eb5d4d9c98d6f02cc4d582934caa4644d165
Badal Kumar <bkbadal7779810465@gmail.com>
2024-10-24 02:55:42 +0530

    delete contact page

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import prisma from '@/lib/prisma';
+export async function POST(request: Request) {
+  try {
+    const { name, email, message } = await request.json()
+
+    if (!name || !email || !message) {
+      return NextResponse.json({ error: 'All fields are required' }, { status: 400 })
+    }
+
+    // Save the contact form data into the database
+    const contact = await prisma.contact.create({
+      data: {
+        name,
+        email,
+        message,
+      },
+    })
+
+    console.log(name, email, message);
+    
+    return NextResponse.json({ success: true, contact }, { status: 201 })
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to submit the form' }, { status: 500 })
+  }
+}
